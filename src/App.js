abbd33cbdc856c49f1154b8e8a3d33ee0271e4e8
Ishara Madushanka <isharamadushanka240@gmail.com>
2022-07-17 23:52:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { NavBar } from './Components/NavBar';
+import { Banner } from './Components/Banner';
+import { Skills } from './Components/Skills';
+import { Projects } from './Components/Projects';
+import 'bootstrap/dist/css/bootstrap.min.css';
+ 
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills /> 
+      <Projects />     
+    </div>
+  );
+}
+
+export default App;
